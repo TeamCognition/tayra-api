235e54c0afc975be58e0ac1a590ef7035472a1e2
almulalic <almir.mulalic.am@gmail.com>
2020-03-13 11:03:57 +0100

    Added advisor controller,service and interface, added overview get, segment view post and grid post view for segment action points, started implementing conclude

--- src/API/Controllers/AdvisorController.cs
+++ src/API/Controllers/AdvisorController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Firdaws.Core;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Tayra.Models.Organizations;
 using Tayra.Services;
 
@@ -14,16 +15,41 @@ namespace Tayra.API.Controllers
             OrganizationContext = dbContext;
         }
 
+        #region Properties
+        protected OrganizationDbContext DbContext { get; set; }
+
+        #endregion
+
         #endregion
 
         protected OrganizationDbContext OrganizationContext;
 
         #region Action Methods
 
-        [HttpPost("search")]
-        public ActionResult<GridData<ActionPointGridDTO>> GetActionPointGrid([FromBody] GridParams gridParams)
+        [HttpGet("overview")]
+        public ActionResult<AdvisorOverviewDTO> GetActionPointOverview()
         {
-            return Ok(AdvisorService.GetActionPointGrid(gridParams));
+            return Ok(AdvisorService.GetActionPointOverview());
+        }
+
+        [HttpPost, Route("{segmentId}/segmentView")]
+        public ActionResult<AdvisorSegmentViewDTO> GetSegmentView([FromRoute] int segmentId)
+        {
+            return Ok(AdvisorService.GetSegmentView(segmentId));
+        }
+
+        [HttpPost, Route("{segmentId}/segmentActionPoints")]
+        public ActionResult<GridData<AdvisorSegmentGridDTO>> GetSegmentActionPointGrid(GridParams gridParams,[FromRoute] int segmentId)
+        {
+            return Ok(AdvisorService.GetSegmentActionPointGrid(gridParams,segmentId));
+        }
+
+        [HttpPut]
+        public void Conclude([FromQuery] int segmentId)
+        {
+            AdvisorService.Conclude(segmentId);
+            DbContext.SaveChanges();
+            return Ok();
         }
 
         #endregion
