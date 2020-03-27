d504b6dcbcc54216befe84fab568f0c391b3b2d9
almulalic <almir.mulalic.am@gmail.com>
2020-03-27 13:25:35 +0100

    Added role to segment member grid

--- src/Services/Segments/DTO/SegmentMemberGridDTO.cs
+++ src/Services/Segments/DTO/SegmentMemberGridDTO.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using Tayra.Common;
+
 namespace Tayra.Services
 {
     public class SegmentMemberGridDTO
     {
         public string Name { get; set; }
         public string Username { get; set; }
+        public ProfileRoles Role { get; set; }
         public string Avatar { get; set; }
         public DateTime MemberFrom { get; set; }
     }
