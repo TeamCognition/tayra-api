00d3a50d1f1e6aae3987d622d272678c4d0d0ead
Fayiz Hamad <e.f.i_cet@hotmail.com>
2021-02-02 18:16:39 +0100

    feat: add giftReceivedSlack template

--- src/Mailer/MailerTemplateModels/GiftReceivedTemplateModel.cs
+++ src/Mailer/MailerTemplateModels/GiftReceivedTemplateModel.cs
@@ -11,6 +11,8 @@ namespace Tayra.Mailer.MailerTemplateModels
         public string SenderName { get; set; }
         
         public string GiftLink { get; set; }
+        
+        public string SlackUserId { get; set; }
 
        public GiftReceivedTemplateModel(string firstName, string senderName,string giftLink)
         {
@@ -18,22 +20,40 @@ namespace Tayra.Mailer.MailerTemplateModels
             SenderName = senderName;
             GiftLink = giftLink;
         }
+
+       public GiftReceivedTemplateModel(string slackUserId, string giftLink)
+       {
+           SlackUserId = $"*<@{slackUserId}>*";
+           GiftLink = giftLink;
+       }
         public  string GetEmailTemplate()
         {
             // string path =Path.(@"TemplatesFiles");
             // Console.WriteLine(path);
-            var engine = new RazorLightEngineBuilder().UseFileSystemProject("C:/Users/efi_c/OneDrive/Desktop/Tayra/Backend/tayra-api/src/Mailer/TemplatesFiles")
+            var engine = new RazorLightEngineBuilder().UseFileSystemProject("C:/Users/TAYRA/Documents/GitHub/tayra-api/src/Mailer/TemplatesFiles/EmailTemplates")
                 .UseMemoryCachingProvider().Build();
             GiftReceivedTemplateModel model = this;
-            Console.WriteLine(model);
             string result =engine.CompileRenderAsync("GiftReceivedTemplate.cshtml", model).GetAwaiter().GetResult();
-            Console.WriteLine(result);
+            Console.WriteLine(AppContext.BaseDirectory);
             return result;
         }
 
         public string GetSlackTemplate()
         {
-            throw new System.NotImplementedException();
+            string template = File.ReadAllText(
+                "C:/Users/TAYRA/Documents/GitHub/tayra-api/src/Mailer/TemplatesFiles/SlackTemplates/GiftReceivedTemplate.json");
+            Console.WriteLine(template);
+            var engine = new RazorLightEngineBuilder()
+                // required to have a default RazorLightProject type,
+                // but not required to create a template from string.
+                .UseEmbeddedResourcesProject(typeof(GiftReceivedTemplateModel))
+                .UseMemoryCachingProvider()
+                .Build(); 
+            //string newString = " hejjjjjjjjjj @UserSlackId and @GiftLink";
+            GiftReceivedTemplateModel model = this;
+            string result =  engine.CompileRenderStringAsync("templateKey", template, model).GetAwaiter().GetResult();
+            Console.WriteLine($"the result is{result}");
+            return result;
         }
     }
 }
