e8ab4b74c4062575c0b591564ea5e7b1fd77980e
Armin Odobašić <armin.odobasic@edu.fit.ba>
2021-09-02 20:28:54 +0200

    implement send email to segment managers in SendEmailReport

--- src/Functions/SendEmailReport/SendEmailReport.cs
+++ src/Functions/SendEmailReport/SendEmailReport.cs
@@ -1,27 +1,110 @@
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using SyncFunctions;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tayra.Common;
 using Tayra.Mailer;
+using Tayra.Models.Catalog;
+using Tayra.Models.Organizations;
 
 namespace Tayra.Functions.SendEmailReport
 {
     public class SendEmailReport
     {
+        private readonly CatalogDbContext _catalogDb;
         private readonly IMailerService _mailerService;
+        private readonly string _emailSubject;
 
-        public SendEmailReport()
+        public SendEmailReport(CatalogDbContext catalogDb)
         {
             _mailerService = new MailerService();
+            _catalogDb = catalogDb;
+            _emailSubject = "Your weekly Tayra report";
         }
 
         [Function(nameof(SendEmailReport))]
-        public void Run([TimerTrigger("*/10 * * * * *")] MyInfo myTimer, FunctionContext context)
+        public async Task RunAsync([TimerTrigger("*/10 * * * * *")] MyInfo myTimer, FunctionContext context)
         {
             var logger = context.GetLogger(nameof(SendEmailReport));
             logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            List<Tenant> tenants = await GetAllTenants();
 
-            _mailerService.SendEmail("armin.odobasic@tayra.io", "Weekly report", "Hi, this is your weekly report.");
+            foreach (var tenant in tenants)
+            {
+                await GenerateAndSendTenantEmails(tenant);
+            }
         }
+
+        #region Private methods
+
+        private async Task<List<Tenant>> GetAllTenants()
+        {
+            return await _catalogDb.TenantInfo.ToListAsync();
+        }
+
+        private async Task<bool> GenerateAndSendTenantEmails(Tenant tenant)
+        {
+            using (var organizationDb = new OrganizationDbContext(tenant, null))
+            {
+                var managerAssignements = await GetAllManagerAssignements(organizationDb);
+
+                var managerIdentities = await GetAllManagerIdentities(managerAssignements);
+
+                foreach (var segmentAssignement in managerAssignements)
+                {
+                    var managerIdentity = GetManagerIdentity(managerIdentities, segmentAssignement);
+                    GenerateAndSendManagerEmail(managerIdentity, segmentAssignement);
+                }
+            }
+
+            return true;
+        }
+
+        private bool GenerateAndSendManagerEmail(IdentityEmail managerIdentity, ProfileAssignment segmentAssignement)
+        {
+            var emailMessage = GenerateEmailMessage(segmentAssignement);
+
+            var response = _mailerService.SendEmail(managerIdentity.Email, _emailSubject, emailMessage);
+
+            return response.StatusCode == System.Net.HttpStatusCode.OK;
+        }
+
+        private IdentityEmail GetManagerIdentity(ICollection<IdentityEmail> managerIdentities, ProfileAssignment segmentAssignement)
+        {
+            return managerIdentities.FirstOrDefault(x => x.IdentityId == segmentAssignement.Profile.IdentityId);
+        }
+
+        private async Task<List<IdentityEmail>> GetAllManagerIdentities(ICollection<ProfileAssignment> managerAssignements)
+        {
+            var managerIdentities = await _catalogDb.IdentityEmails.Where(x => managerAssignements.Select(y => y.Profile.IdentityId)
+                                                                                                  .Contains(x.IdentityId))
+                                                                   .ToListAsync();
+
+            return managerIdentities;
+        }
+
+        private async Task<List<ProfileAssignment>> GetAllManagerAssignements(OrganizationDbContext organizationDb)
+        {
+            var profileAssignements = await organizationDb.ProfileAssignments.Include(x => x.Segment)
+                                                                             .Include(x => x.Profile)
+                                                                             .Where(x => x.Profile.Role == ProfileRoles.Manager)
+                                                                             .ToListAsync();
+
+            return profileAssignements;
+        }
+
+        private string GenerateEmailMessage(ProfileAssignment profileAssignment)
+        {
+            string message = $"Hello {profileAssignment.Profile?.FullName}, this is your weekly report for segment {profileAssignment.Segment?.Name}";
+
+            return message;
+        }
+
+        #endregion
     }
 
     public class MyInfo
